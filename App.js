6e36ada0d93b38e59a92b55ddf59a60fe21015d5
lhjwork <jin.come.up.business@gmail.com>
2022-01-26 15:28:36 +0900

    Provider error 수정

--- App.js
+++ App.js
@@ -2,11 +2,19 @@ import React from 'react';
 import {View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import RootStack from './src/navigations/RootStack';
+import {PersistGate} from 'redux-persist/integration/react';
+import store, {persistor} from './src/redux/store';
+import {Provider} from 'react-redux';
+
 const App = () => {
   return (
-    <NavigationContainer>
-      <RootStack />
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <NavigationContainer>
+          <RootStack />
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 };
 
