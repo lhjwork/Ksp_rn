7ee94ccbbd97b4c760de2b85a8ad4ec4f84fcf31
lhjwork <jin.come.up.business@gmail.com>
2022-01-28 14:59:19 +0900

    error 수정

--- android/app/src/main/java/com/ksp_rn/MainApplication.java
+++ android/app/src/main/java/com/ksp_rn/MainApplication.java
@@ -13,6 +13,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.facebook.react.bridge.JSIModulePackage; // <- add this
+import com.swmansion.reanimated.ReanimatedJSIModulePackage; // <- add this
 
 
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -40,6 +42,10 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSMainModuleName() {
           return "index";
         }
+        @Override 
+        protected JSIModulePackage getJSIModulePackage() {
+        return new ReanimatedJSIModulePackage(); // <- add
+      }
 
       };
 
