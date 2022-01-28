0890bca9a38bd786a8ac0704043103bf9e0c744b
lhjwork <jin.come.up.business@gmail.com>
2022-01-28 16:23:38 +0900

    variable onPressDrawer

--- src/components/HeaderCompnent.js
+++ src/components/HeaderCompnent.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import {View, Image, Text} from 'react-native';
+import {View, Image, Text, StyleSheet} from 'react-native';
 import RowView from './Views/RowView';
+import Touchable from '../components/Touchable';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 
-const HeaderCompnent = () => {
+const HeaderCompnent = ({navigation, onPerssDrawer}) => {
   return (
-    <RowView>
-      <Text>ssss</Text>
+    <RowView style={styles.headerRow}>
+      <Touchable>
+        <AntDesign name={'left'} size={17} style={{marginLeft: 27.5}} />
+      </Touchable>
+      <Touchable onPress={onPerssDrawer}>
+        <Image
+          source={require('../asssets/icons/headerRight_more.png')}
+          style={{marginRight: 27}}
+        />
+      </Touchable>
     </RowView>
   );
 };
 
 export default HeaderCompnent;
+
+const styles = StyleSheet.create({
+  headerRow: {justifyContent: 'space-between'},
+});
--- src/screens/ShoppingMall/ShoppingMall.js
+++ src/screens/ShoppingMall/ShoppingMall.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import HeaderCompnent from '../../components/HeaderCompnent';
+import Touchable from '../../components/Touchable';
 
 const ShoppingMall = ({navigation}) => {
   return (
