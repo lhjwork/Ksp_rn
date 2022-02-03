7fb7358e9148c312df7125767a9a76083420cf5a
lhjwork <jin.come.up.business@gmail.com>
2022-02-03 10:42:25 +0900

    ScrollView

--- src/screens/ShoppingMall/ShoppingMall.js
+++ src/screens/ShoppingMall/ShoppingMall.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import {View, Text, StyleSheet, FlatList, Image} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Image,
+  ScrollView,
+} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import HeaderCompnent from '../../components/HeaderCompnent';
 import Touchable from '../../components/Touchable';
@@ -20,72 +27,76 @@ const ShoppingMall = ({navigation}) => {
   console.log('ddddd');
   return (
     <LinearGradient colors={['#91C7D6', '#CBE2DC']} style={{flex: 1}}>
-      <HeaderCompnent onPerssDrawer={() => navigation.openDrawer()} />
+      <ScrollView>
+        <HeaderCompnent onPerssDrawer={() => navigation.openDrawer()} />
 
-      <ContainerStyled>
-        <View style={{marginHorizontal: 30}}>
-          <BoldLabelTitle text={'코나 쇼핑몰'} style={{marginTop: 27.5}} />
-          <BoldLabelSubTitle
-            text={'TOTAL PORTFOLIL VALUE'}
-            style={{marginTop: 13}}
-          />
-        </View>
-        <View style={styles.adverBanner}></View>
-        <View style={{marginHorizontal: 30}}>
-          <FlatList
-            data={ShoppingMallData}
-            showsVerticalScrollIndicator={false}
-            keyExtractor={(item, index) => item.id + index.toString()}
-            numColumns={3}
-            renderItem={({item, index}) => {
-              console.log(item);
-              return (
-                <ColumnView>
-                  <View
-                    style={{
-                      width: 80,
-                      height: 80,
-                      borderRadius: 40,
-                      backgroundColor: '#fff',
-                      marginLeft: index % 3 === 0 ? 0 : 37,
-                    }}>
-                    <Image />
-                  </View>
-                  <LabelNone
-                    style={{
-                      marginLeft: index % 3 === 0 ? 0 : 37,
-                      marginTop: 9.61,
-                      marginBottom: 26.63,
-                    }}
-                    text={'dddd'}
-                  />
-                </ColumnView>
-              );
-            }}
-          />
+        <ContainerStyled>
+          <View style={{marginHorizontal: 30}}>
+            <BoldLabelTitle text={'코나 쇼핑몰'} style={{marginTop: 27.5}} />
+            <BoldLabelSubTitle
+              text={'TOTAL PORTFOLIL VALUE'}
+              style={{marginTop: 13}}
+            />
+          </View>
+          <View style={styles.adverBanner}></View>
+          <View style={{marginHorizontal: 30}}>
+            <FlatList
+              data={ShoppingMallData}
+              showsVerticalScrollIndicator={false}
+              keyExtractor={(item, index) => item.id + index.toString()}
+              numColumns={3}
+              renderItem={({item, index}) => {
+                console.log(item);
+                return (
+                  <ColumnView>
+                    <View
+                      style={{
+                        width: 80,
+                        height: 80,
+                        borderRadius: 40,
+                        backgroundColor: '#fff',
+                        marginLeft: index % 3 === 0 ? 0 : 37,
+                      }}>
+                      <Image />
+                    </View>
+                    <LabelNone
+                      style={{
+                        marginLeft: index % 3 === 0 ? 0 : 37,
+                        marginTop: 9.61,
+                        marginBottom: 26.63,
+                      }}
+                      text={'dddd'}
+                    />
+                  </ColumnView>
+                );
+              }}
+            />
 
-          <LinearGradient
-            colors={['#91C7D6', '#CBE2DC']}
-            style={{
-              height: 40,
-              justifyContent: 'center',
-            }}>
-            <Touchable
+            <LinearGradient
+              colors={['#91C7D6', '#CBE2DC']}
               style={{
-                height: 38,
-                backgroundColor: '#fff',
-                marginHorizontal: 2,
+                height: 40,
                 justifyContent: 'center',
-                alignItems: 'center',
+                marginTop: 40.41,
+                marginBottom: 30,
               }}>
-              <LabelNone
-                text={'코나 쇼핑몰 홈페이지 바로가기'}
-                style={{color: '#555555', fontSize: 12, Lineheight: 17.38}}
-              />
-            </Touchable>
-          </LinearGradient>
-        </View>
-      </ContainerStyled>
+              <Touchable
+                style={{
+                  height: 38,
+                  backgroundColor: '#fff',
+                  marginHorizontal: 2,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <LabelNone
+                  text={'코나 쇼핑몰 홈페이지 바로가기'}
+                  style={{color: '#555555', fontSize: 12, Lineheight: 17.38}}
+                />
+              </Touchable>
+            </LinearGradient>
+          </View>
+        </ContainerStyled>
+      </ScrollView>
     </LinearGradient>
   );
 };
