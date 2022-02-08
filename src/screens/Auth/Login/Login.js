5970f350f70fa7d344c130e346a994931ae8ebd7
lhjwork <jin.come.up.business@gmail.com>
2022-02-08 11:45:39 +0900

    styled

--- src/screens/Auth/Login/Login.js
+++ src/screens/Auth/Login/Login.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import styled from 'styled-components/native';
 
 const Login = () => {
   return (
-    <LinearGradient
-      colors={['#91C7D6', '#CBE2DC']}
-      style={{flex: 1}}></LinearGradient>
+    <LinearGradient colors={['#91C7D6', '#CBE2DC']} style={{flex: 1}}>
+      <Logo />
+    </LinearGradient>
   );
 };
 
 export default Login;
+
+const Logo = styled.Image``;
