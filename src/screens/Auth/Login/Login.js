4de89a3d86a157e24fdf975ce3e18f6442bbf314
lhjwork <jin.come.up.business@gmail.com>
2022-02-08 11:42:33 +0900

    Login 그라데이션

--- src/screens/Auth/Login/Login.js
+++ src/screens/Auth/Login/Login.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 const Login = () => {
   return (
-    <View>
-      <Text>Login.js</Text>
-    </View>
+    <LinearGradient
+      colors={['#91C7D6', '#CBE2DC']}
+      style={{flex: 1}}></LinearGradient>
   );
 };
 
