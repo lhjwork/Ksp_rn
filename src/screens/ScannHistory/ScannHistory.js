f5babb8726fe032ff6990fe36bbc40382eef4204
lhjwork <jin.come.up.business@gmail.com>
2022-02-21 16:42:00 +0900

    스캔 히스토리 View 헤더,Title 연결

--- src/screens/ScannHistory/ScannHistory.js
+++ src/screens/ScannHistory/ScannHistory.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import HeaderCompnent from '../../components/HeaderCompnent';
+import {BoldLabelTitle} from '../../components/Labels';
 
-const ScannHistory = () => {
+const ScannHistory = ({navigation}) => {
   return (
     <LinearGradient colors={['#91C7D6', '#CBE2DC']} style={{flex: 1}}>
-      <Text>스캔히스토리</Text>
+      <HeaderCompnent onPerssDrawer={() => navigation.openDrawer()} />
+      <View style={{marginHorizontal: 24, flex: 1}}>
+        <BoldLabelTitle text={'스캔 히스토리'} style={{marginTop: 27.5}} />
+      </View>
     </LinearGradient>
   );
 };
