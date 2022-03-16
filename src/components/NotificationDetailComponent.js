89fd5d67ad1df21c4a12995ae39ffd873cd2d62e
lhjwork <jin.come.up.business@gmail.com>
2022-03-16 14:35:17 +0900

    공지사항 디테일 컴포넌트

---
+++ src/components/NotificationDetailComponent.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import {BoldLabel14, LabelNone} from './Labels';
+import Touchable from './Touchable';
+import RowView from './Views/RowView';
+
+const NotificationDetailComponent = ({outStyle, text, date}) => {
+  return (
+    <View
+      style={{
+        height: 76,
+        marginHorizontal: 24,
+        borderBottomWidth: 1,
+        borderBottomColor: '#E5E5E5',
+        ...outStyle,
+      }}>
+      <Touchable>
+        <RowView style={{paddingTop: 20}}>
+          <View style={styles.bar} />
+          <BoldLabel14 text={text} style={{color: '#555'}} />
+        </RowView>
+        <LabelNone text={date} style={styles.date} />
+      </Touchable>
+    </View>
+  );
+};
+
+export default NotificationDetailComponent;
+
+const styles = StyleSheet.create({
+  date: {textAlign: 'right', marginTop: 9, color: '#46A0BD'},
+  bar: {
+    backgroundColor: '#46A0BD',
+    width: 5,
+    height: 12,
+    marginRight: 10,
+  },
+});
--- src/screens/Notification/Notification.js
+++ src/screens/Notification/Notification.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import HeaderCompnent from '../../components/HeaderCompnent';
+import {ContainerStyled} from '../../components/StyledComponents/StyledComponents';
+import {BoldLabelTitle} from '../../components/Labels';
+import NotificationDetailComponent from '../../components/NotificationDetailComponent';
 const Notification = () => {
   return (
-    <LinearGradient
-      colors={['#91C7D6', '#CBE2DC']}
-      style={{flex: 1}}></LinearGradient>
+    <LinearGradient colors={['#91C7D6', '#CBE2DC']} style={{flex: 1}}>
+      <HeaderCompnent
+        onPerssDrawer={() => navigation.openDrawer()}
+        onPressLeftBtn={() => navigation.goBack()}
+      />
+      <ContainerStyled>
+        <BoldLabelTitle
+          text={'공지사항'}
+          style={{marginTop: 27.5, marginHorizontal: 30, marginBottom: 55}}
+        />
+        <ContainerStyled style={{backgroundColor: '#fff', elevation: 3}}>
+          <NotificationDetailComponent />
+        </ContainerStyled>
+      </ContainerStyled>
+    </LinearGradient>
   );
 };
 
